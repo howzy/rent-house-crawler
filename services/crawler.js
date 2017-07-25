effb8666a0e10c41c2e4eb6c4777091dcb8edb4b
once <zhonce829@gmail.com>
2017-07-26 06:42:42 +0800

    获取豆瓣租房详情页信息

--- services/crawler.js
+++ services/crawler.js
@@ -5,13 +5,13 @@ const cheerio = require('cheerio');
 function fetchSingleDoubanList(start) {
   axios.get(`https://www.douban.com/group/HZhome/discussion?start=${start}`)
     .then(res => {
-      // console.log(res);
       let htmlText = res.data;
 
       const $ = cheerio.load(htmlText);
       const rs = $('a[title]');
       const resultList = [];
 
+      // 提取列表页中每项的标题和链接
       for (let i = 0; i < rs.length; i++) {
         resultList.push({
           title: rs.eq(i).attr('title'),
@@ -19,7 +19,6 @@ function fetchSingleDoubanList(start) {
         })
       }
 
-      // console.log(resultList);
       return resultList;
     })
     .catch(err => {
@@ -27,6 +26,39 @@ function fetchSingleDoubanList(start) {
     })
 }
 
+// 获取豆瓣租房详情页信息
+function fetchSingleDoubanTopic(url) {
+  axios.get(url)
+    .then(res => {
+      let htmlText = res.data;
+
+      const $ = cheerio.load(htmlText);
+      const ps = $('.topic-content > p');
+      const topicPics = $('.topic-content > img');
+      const details = [];
+      const pics = [];
+
+      // 提取详情页中的描述文字
+      for (let i = 0; i < ps.length; i++) {
+        details.push(ps.eq(i).text());
+      }
+
+      // 提取详情页中的展示图片
+      for (let i = 0; i < topicPics.length; i++) {
+        pics.push(topicPics.eq(i).attr('src'));
+      }
+
+      return {
+        details,
+        pics
+      }
+    })
+    .catch(err => {
+      console.log(err);
+    })
+}
+
 module.exports = {
-  fetchSingDoubanList: fetchSingDoubanList
+  fetchSingleDoubanList,
+  fetchSingleDoubanTopic
 }
