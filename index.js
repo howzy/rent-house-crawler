5c5b93ea762a582392bc0028183f1afa8b825f58
once <zhonce829@gmail.com>
2017-07-31 20:12:22 +0800

    对于符合条件的 topic 去获取它的详情信息，并更新到数据库

--- index.js
+++ index.js
@@ -5,23 +5,27 @@ const target = '东站';
 
 for (let i = 0; i < 100; i += 25) {
   crawler.fetchSingleDoubanList(i)
-    .then(res => {
-      for (let j = 0; j < res.length; j++) {
-        Topic.create(res[j])
+    .then(topicList => {
+      for (let j = 0; j < topicList.length; j++) {
+        Topic.create(topicList[j])
           .then(r => {
             if (isNear(r.title, target)) {
-              crawler.fetchSingleDoubanTopic(r.url);
+              crawler.fetchSingleDoubanTopic(r.url)
+                .then(singleTopic => {
+                  Topic.update({ url: r.url }, { $set: { details: singleTopic.details, pics: singleTopic.pics } });
+                })
             }
           })
           .catch(e => {
             if (e.message.match('E11000 duplicate key')) {
-             console.log('重复数据');
+              console.log('重复数据');
             }
           })
       }
     })
 }
 
+// 判断给定的位置中是否含有目标关键词
 function isNear(location, target) {
   return location.indexOf(target) > -1;
 }
