3bbae42b51ff092b3674782e6d12475c63435eca
once <zhonce829@gmail.com>
2017-08-03 10:02:02 +0800

    完善关键词过滤函数，开启定时爬取

--- index.js
+++ index.js
@@ -2,38 +2,44 @@ require('./services/mongo');
 const mailer = require('./services/mailer');
 const Topic = require('./models/topic');
 const crawler = require('./services/crawler');
-const target = '城西';
+const target = ['城西'];
 
-let filterTopicList = []; // 最终筛选过后的详情页信息列表
-
-for (let i = 0; i < 100; i += 25) {
-  crawler.fetchSingleDoubanList(i)
-    .then(topicList => {
-      for (let j = 0; j < topicList.length; j++) {
-        Topic.create(topicList[j])
-          .then(r => {
-            if (isNear(r.title, target)) {
-              crawler.fetchSingleDoubanTopic(r.url)
-                .then(singleTopic => {
-                  Topic.findOneAndUpdate({ url: r.url }, { $set: { createTime: singleTopic.createTime, details: singleTopic.details, pics: singleTopic.pics } }, { new: true })
-                    .then(rr => {
-                      mailer.sendMail(rr);
-                    });
-                })
-            }
-          })
-          .catch(e => {
-            if (e.message.match('E11000 duplicate key')) {
-              console.log('重复数据');
-            }
-          })
-      }
-    })
+function crawlerTopic() {
+  for (let i = 0; i < 850; i += 25) {
+    crawler.fetchSingleDoubanList(i)
+      .then(topicList => {
+        for (let j = 0; j < topicList.length; j++) {
+          Topic.create(topicList[j])
+            .then(r => {
+              if (isNear(r.title, target)) {
+                crawler.fetchSingleDoubanTopic(r.url)
+                  .then(singleTopic => {
+                    Topic.findOneAndUpdate({ url: r.url }, { $set: { createTime: singleTopic.createTime, details: singleTopic.details, pics: singleTopic.pics } }, { new: true })
+                      .then(rr => {
+                        mailer.sendMail(rr);
+                      });
+                  })
+              }
+            })
+            .catch(e => {
+              if (e.message.match('E11000 duplicate key')) {
+                console.log('重复数据');
+              }
+            })
+        }
+      })
+  }
 }
 
-console.log(filterTopicList);
+var count = 0;
+crawlerTopic();
+setInterval(function () {
+  crawlerTopic();
+  console.log(++count);
+}, 60000);
 
 // 判断给定的位置中是否含有目标关键词
 function isNear(location, target) {
-  return location.indexOf(target) > -1;
+  let patt = new RegExp(target.join('|'));
+  return patt.test(location);
 }
